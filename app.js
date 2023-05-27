17932cdf8b600f9def59607b122f0ac3d3e701fa
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-05-27 20:19:47 +0530

    Middleware work done

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const express=require('express');
+
+const app = express();
+app.use((req,res,next)=>{
+    console.log('it is a middleware')
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log('it is a middleware second')
+    res.send('<h1> Hello from Express..!</h1>')
+})
+
+app.listen(3000);
