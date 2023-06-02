cd1d713856505e80b0f70d994538dc10182a1ccd
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-06-02 16:24:28 +0530

    Models done

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const productcontroller=require('../Controllers/products')
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/',productcontroller.getproduct );
+router.get('/', productsController.getProducts);
 
 module.exports = router;
